422c701ff4ab70c6840a85a53005ccd719ab4f4d
Cameron L. <152921703+ItsKingzy@users.noreply.github.com>
2026-04-26 15:21:54 -0600

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        # Make alnum arr
+        alnumArr = []
+        for char in s:
+            if char.isalnum():
+                alnumArr.append(char.lower())
+        
+        # Initialize 2 pointers
+        p1 = 0 # First element
+        p2 = len(alnumArr) - 1 # Last element
+
+        # Loop through to see if is palindrome
+        for i in range(len(alnumArr)):
+            if (p1 != p2 or p2 - p1 != 1):
+                if (alnumArr[p1] != alnumArr[p2]):
+                    return False
+                # Increment pointers
+                p1 += 1
+                p2 -= 1
+        
+        return True
+
+
+        
